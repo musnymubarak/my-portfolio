31d3425c168a3711219b6cc691419e1590d4ee7a
musnymubarak <musnymohammed@gmail.com>
2024-07-18 13:55:57 +0530

    Hero Componenet

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,18 +15,17 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold drop-shadow-lg">
+            <span className="block text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+              Hello, I&apos;m
             </span>
-            <br></br>
             <TypeAnimation
               sequence={[
                 "Musny Mubarak",
                 1000,
-                "Full-Stack Developer",
+                "Web Developer",
                 1000,
-                "Mobile App Developer",
+                "Mobile Developer",
                 1000,
                 "UI/UX Designer",
                 1000,
@@ -43,15 +42,15 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:bg-gradient-to-l hover:from-purple-600 hover:via-pink-600 hover:to-red-600 text-white transition duration-300"
             >
               Hire Me
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 hover:bg-gradient-to-l hover:from-green-500 hover:via-blue-600 hover:to-purple-700 text-white mt-3 transition duration-300"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-[#0e0e0e] rounded-full px-5 py-2">
                 Download CV
               </span>
             </Link>
@@ -63,7 +62,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[350px] h-[350px] lg:w-[500px] lg:h-[500px] relative">
+          <div className="rounded-full bg-[#202020] w-[350px] h-[350px] lg:w-[500px] lg:h-[500px] relative border-2 border-primary-500">
             <Image
               src="/images/Musny.png"
               alt="Musny image"
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black container mx-auto px-12 py-10">
-    <HeroSection/>
+
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
