56d99384e70d04752d4d4a7742bf6113838d83ee
musnymubarak <musnymohammed@gmail.com>
2024-07-18 15:22:36 +0530

    About Me

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import AboutMe from "./components/AboutMe"; 
 
 export default function Home() {
   return (
