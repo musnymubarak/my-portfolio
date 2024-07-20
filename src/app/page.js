07e7494ab24066e35e3ef9862bfaa1b7d73d00a0
musnymubarak <musnymohammed@gmail.com>
2024-07-20 12:06:45 +0530

    Error correction

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import EmailSection from "./components/EmailSection";
